411dde72bd8d80627333e5130a62ad5c94b22c02
Kristine Adzema <kadzema@gmail.com>
2017-09-20 20:18:55 -0400

    uncomment the code that posts

--- kadzemaPlotBot.py
+++ kadzemaPlotBot.py
@@ -55,7 +55,7 @@ def TweetOut(user, requester, replyID, avgSentiment):
     print("length of tweet: " + str(len(tweetreply)))
 
     try:
-        # api.update_with_media(graph, tweetreply, in_reply_to_status_id =replyID )
+        api.update_with_media(graph, tweetreply, in_reply_to_status_id =replyID )
         print(tweetreply)
     except:
         print("update with media error")
